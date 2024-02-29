09cb28dd9f1c76d8bf0351e1ad5a180c55f05ed3
shubhamshoor <shoorshubham1@gmail.com>
2024-02-29 13:05:06 +0530

    hello

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\shoor\\Desktop\\travel\\project_next13_car_showcase\\app\\layout.tsx": [
+      "C:\\Users\\shoor\\Desktop\\travel\\project_next13_car_showcase\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\shoor\\Desktop\\travel\\project_next13_car_showcase\\app\\page": [
+      "C:\\Users\\shoor\\Desktop\\travel\\project_next13_car_showcase\\app\\globals.css"
+    ]
+  }
+}
